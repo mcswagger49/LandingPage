4b3a67dfae364e00484603e68de878e00355b6ec
Joshua Smith <joshsmith0419@gmail.com>
2015-06-01 10:10:01 -0700

    Starwars css

--- icons.php
+++ icons.php
@@ -54,3 +54,14 @@
     </div>
   </div>
 </div>
+
+<div id="bg">
+  <div id="titles">
+    <div id="titlecontent">
+
+      <p>How cool is that? No JavaScript, no graphics — just pure HTML5 text and CSS3. It works in Chrome, Safari and Firefox. Opera won’t show the 3D transform in the current release, but it’ll work. Internet Explorer. Hmmm. Sorry, but you’ll need to wait
+        for IE10 since previous versions don’t support transformations and animations. The effect is remarkably easy to implement. The HTML requires an outer element (#titles) and a section which will be scrolled (#titlecontent):</p>
+
+    </div>
+  </div>
+</div>
