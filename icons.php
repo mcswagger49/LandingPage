df786786b5a14f2abfddecbcc34136aa3dbec9fb
Joshua Smith <joshsmith0419@gmail.com>
2015-05-31 22:43:50 -0700

    my file for my icons

---
+++ icons.php
@@ -0,0 +1,56 @@
+<div class="share-buttons">
+  <div class="share-button">
+    <div class="share-button-secondary">
+      <div class="share-button-secondary-content">
+        share on twitter
+      </div>
+    </div>
+    <div class="share-button-primary">
+      <i class="share-button-icon fa fa-twitter"></i>
+    </div>
+  </div>
+
+  <div class="share-button">
+    <div class="share-button-secondary">
+      <div class="share-button-secondary-content">
+        share on facebook
+      </div>
+    </div>
+    <div class="share-button-primary">
+      <i class="share-button-icon fa fa-facebook"></i>
+    </div>
+  </div>
+
+  <div class="share-button">
+    <div class="share-button-secondary">
+      <div class="share-button-secondary-content">
+        pin on pinterest
+      </div>
+    </div>
+    <div class="share-button-primary">
+      <i class="share-button-icon fa fa-pinterest"></i>
+    </div>
+  </div>
+
+  <div class="share-button">
+    <div class="share-button-secondary">
+      <div class="share-button-secondary-content">
+        share on tumblr
+      </div>
+    </div>
+    <div class="share-button-primary">
+      <i class="share-button-icon fa fa-tumblr"></i>
+    </div>
+  </div>
+
+  <div class="share-button">
+    <div class="share-button-secondary">
+      <div class="share-button-secondary-content">
+        share on google+
+      </div>
+    </div>
+    <div class="share-button-primary">
+      <i class="share-button-icon fa fa-google-plus"></i>
+    </div>
+  </div>
+</div>
